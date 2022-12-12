609c670527337169445a60be7c530377b6ee7561
quantingxie <65790798+quantingxie@users.noreply.github.com>
2022-12-12 12:29:41 -0500

    Add files via upload

---
+++ script_version/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='rob831',
+    version='0.1.0',
+    packages=find_packages(),
+    package_data={'':['*.xml']},
+    include_package_data=True
+)
